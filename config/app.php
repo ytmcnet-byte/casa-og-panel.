c91ea0073646afb451d471337d4aba4e251307d3
ytmcnet-byte <ytmc.net@gmail.com>
2026-02-19 21:53:34 +0530

    Update app.php

--- config/app.php
+++ config/app.php
@@ -23,7 +23,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Pterodactyl'),
+    'name' => env('APP_NAME', 'casa-og-panel'),
 
     /*
     |--------------------------------------------------------------------------
