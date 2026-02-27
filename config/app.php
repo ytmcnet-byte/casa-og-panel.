40375cecab5417163ff8f15ee3a866ba9a5923e5
ytmcnet-byte <ytmc.net@gmail.com>
2026-02-27 13:32:14 +0530

    Update application version and name in config

--- config/app.php
+++ config/app.php
@@ -10,7 +10,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => 'canary',
+    'version' => '1.0',
 
     /*
     |--------------------------------------------------------------------------
@@ -23,7 +23,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'casa-og-panel'),
+    'name' => env('APP_NAME', 'Casaogpanel'),
 
     /*
     |--------------------------------------------------------------------------
